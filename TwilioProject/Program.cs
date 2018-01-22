e2926e4612c7bbe1c5662cc783ef69323acbac0f
Daniel Cheng and Patrick Napper <daniel@>
2018-01-22 09:50:07 -0800

    Display data in readable format from Json String

--- TwilioProject/Program.cs
+++ TwilioProject/Program.cs
@@ -4,9 +4,19 @@ using RestSharp;
 using RestSharp.Authenticators;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace TwilioProject
 {
+    public class Message
+    {
+        public string To { get; set; }
+        public string From { get; set; }
+        public string Body { get; set; }
+        public string Status { get; set; }
+        public string Price { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -24,17 +34,27 @@ namespace TwilioProject
             }).Wait();
             //4
             JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
-            Console.WriteLine(jsonResponse["messages"]);
+            List<Message> messageList = JsonConvert.DeserializeObject<List<Message>>(jsonResponse["messages"].ToString());
+            foreach (Message message in messageList)
+            {
+                Console.WriteLine("To: {0}", message.To);
+                Console.WriteLine("From: {0}", message.From);
+                Console.WriteLine("Body: {0}", message.Body);
+                Console.WriteLine("Status: {0}", message.Status);
+                Console.WriteLine("Price: {0}", message.Price);
+            }
             Console.ReadLine();
         }
 
         public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
         {
             var tcs = new TaskCompletionSource<IRestResponse>();
-            theClient.ExecuteAsync(theRequest, response => {
+            theClient.ExecuteAsync(theRequest, response =>
+            {
                 tcs.SetResult(response);
             });
             return tcs.Task;
         }
     }
+
 }
