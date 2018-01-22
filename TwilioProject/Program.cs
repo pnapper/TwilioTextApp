264e39f9ee672185821c2bb4a538ad7d376e5a41
Daniel Cheng and Patrick Napper <daniel@>
2018-01-22 09:35:14 -0800

    deserialises into json format

--- TwilioProject/Program.cs
+++ TwilioProject/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Threading.Tasks;
 using RestSharp;
 using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace TwilioProject
 {
@@ -8,22 +11,30 @@ namespace TwilioProject
     {
         static void Main(string[] args)
         {
-            //1
             var client = new RestClient("https://api.twilio.com/2010-04-01");
-            //2
-            var request = new RestRequest("Accounts/AC29fe9bc311b4b004b07a9eacdaf14f44/Messages", Method.POST);
-            //3
-            request.AddParameter("To", "+12062293453");
-            request.AddParameter("From", "+19096751391 ");
-            request.AddParameter("Body", "Hello world!");
-            //4
+            //1
+            var request = new RestRequest("Accounts/AC29fe9bc311b4b004b07a9eacdaf14f44/Messages.json", Method.GET);
             client.Authenticator = new HttpBasicAuthenticator("AC29fe9bc311b4b004b07a9eacdaf14f44", "12c6af9e9f5a01b0abba01cd78f761a2");
-            //5
-            client.ExecuteAsync(request, response =>
+            //2
+            var response = new RestResponse();
+            //3a
+            Task.Run(async () =>
             {
-                Console.WriteLine(response);
-            });
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            //4
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            Console.WriteLine(jsonResponse["messages"]);
             Console.ReadLine();
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
         }
     }
 }
